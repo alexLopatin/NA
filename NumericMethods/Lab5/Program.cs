a56e9820f293bfa657345c4b2d3c539a69c3dfc0
alexLopatin <minelabory@gmail.com>
2020-10-07 13:29:21 +0300

    init

--- NumericMethods/Lab5/Program.cs
+++ NumericMethods/Lab5/Program.cs
@@ -44,31 +44,30 @@ namespace Lab5
 		{
 			var conditions = new BoundaryConditionsThirdDegree()
 			{
-				FirstConditionParameters = new[] { 1.0d, 0.0d },
+				FirstConditionParameters = new[] { .0d, 1.0d },
 				SecondConditionParameters = new[] { .0d, 1.0d },
-				InitialFunc = (x, t) => Math.Sin(x),
-				FirstFunc = (x, t) => Math.Exp(-2 * t),
-				SecondFunc = (x, t) => Math.Exp(-2 * t)
+				InitialFunc = (x, t) => Math.Sin(2 * Math.PI * x),
+				FirstFunc = (x, t) => 0,
+				SecondFunc = (x, t) => 0
 			};
 
 			var @params = new FiniteDifferenceParams()
 			{
 				SpaceBoundLeft = 0,
-				SpaceBoundRight = Math.PI / 2,
+				SpaceBoundRight = 1,
 				TimeLimit = 1d,
-				SpaceStepCount = 30,
-				TimeStepCount = 9000,
-				Approximation = Approximation.TwoDotsSecondDegree
+				SpaceStepCount = 200,
+				TimeStepCount = 90000
 			};
 
-			var method = new FiniteDifference(conditions, @params);
+			//var method = new FiniteDifference(conditions, @params);
 
-			var result = method.SolveExplicit(new[] { 1.0d, .0d, -1.0d }, (x, t) => 0);
-			//var method = new CrankNikolsonMethod(conditions, @params);
+			//var result = method.SolveExplicit(new[] { 1.0d, .0d, .0d }, (x, t) => 0);
+			var method = new CrankNikolsonMethod(conditions, @params);
 
-			//var result = method.Solve(new[] { 1.0d, .0d, .0d }, (x, t) => 0);
+			var result = method.Solve(new[] { 1.0d, .0d, .0d }, (x, t) => 0);
 
-			var errors = method.FindError((x, t) => Math.Exp( -2 * t) * Math.Sin(x));
+			var errors = method.FindError((x, t) => Math.Exp(-t * 4 * Math.PI * Math.PI) * Math.Sin(2 * Math.PI * x));
 
 			var maxError = FindMax(errors);
 			var median = FindMedian(errors);
