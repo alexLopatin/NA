3351cc52ab2578d11ef86c1ddedf08e7a5a48ffa
alexLopatin <minelabory@gmail.com>
2020-10-09 23:37:10 +0300

    создал проект

---
+++ NumericMethods/WebPresentation/Server/Controllers/AuthController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Security.Claims;
+using WebPresentation.Server.ReadModels;
+
+namespace WebPresentation.Server.Controllers
+{
+	[ApiController]
+	public class AuthController : ControllerBase
+	{
+		[HttpPost]
+		public async Task<ActionResult> Login([FromBody] LoginReadModel readModel)
+		{
+			var claims = new List<Claim>
+			{
+				new Claim(ClaimsIdentity.DefaultNameClaimType, "ADMIN")
+			};
+			ClaimsIdentity id = new ClaimsIdentity(claims, "ApplicationCookie", ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType);
+
+			await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(id));
+
+			return Redirect("/");
+		}
+	}
+}
---
+++ NumericMethods/WebPresentation/Server/ReadModels/LoginReadModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebPresentation.Server.ReadModels
+{
+	public class LoginReadModel
+	{
+		public string Password { get; set; }
+	}
+}
