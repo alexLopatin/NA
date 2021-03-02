cdb391d9d861412ab384d5f1b655109111e7750a
alexLopatin <minelabory@gmail.com>
2021-03-03 01:13:17 +0300

    lab 7 plot

--- NumericMethods/Lab7/Program.cs
+++ NumericMethods/Lab7/Program.cs
@@ -3,6 +3,11 @@ using NumericMethods.Core.PartialDiffEquation.Elliptical;
 using System;
 using System.Collections.Generic;
 
+using ILNumerics;
+using static ILNumerics.ILMath;
+using static ILNumerics.Globals;
+using System.Linq;
+
 namespace Lab7
 {
 	class Program
@@ -26,62 +31,74 @@ namespace Lab7
 
 		static double FindMax(double[,] grid)
 		{
-			var max = 0.0d;
+			//var max = 0.0d;
+			var ans = 0.0d;
 
 			for (int i = 0; i < grid.GetLength(0); i++)
 			{
 				for (int j = 0; j < grid.GetLength(1); j++)
 				{
-					max = Math.Max(max, grid[i, j]);
+					//max = Math.Max(max, grid[i, j]);
+					ans += grid[i, j] * grid[i, j];
 				}
 			}
 
-			return max;
+			return ans / (grid.GetLength(0) * grid.GetLength(1));
 		}
 
 		static void Main(string[] args)
 		{
 			var conditions = new EllipticalBoundaryConditions()
 			{
-				ConditionParameters = new double[4,2] { { 0, 1 }, { 0, 1 }, { 1, 0 }, { 1, -1 } },
+				ConditionParameters = new double[4,2] { { 1, 0 }, { 1, 0 }, { 0, 1 }, { 0, 1 } },
 				InitialConditions = new Func<double, double, double>[4]
 				{
-					(x, y) => 0,
-					(x, y) => y,
+					(x, y) => Math.Exp(y),
+					(x, y) => -Math.Exp(y),
 					(x, y) => Math.Sin(x),
-					(x, y) => 0
+					(x, y) => Math.E * Math.Sin(x)
 				}
 			};
 			var equation = new EllipticalEquationParams()
 			{
 				a = .0d,
 				b = .0d,
-				c = -1.0d,
+				c = .0d,
 				f = (x, y) => 0
 			};
 			var @params = new EllipticalFiniteDifferenceParams()
 			{
 				XBoundLeft = 0,
-				XBoundRight = Math.PI / 2,
+				XBoundRight = Math.PI,
 				YBoundLeft = 0,
 				YBoundRight = 1d,
 				XStepCount = 80,
 				YStepCount = 80,
-				Solver = SolverType.OverRelaxation,
-				Eps = 0.0000001d
+				Solver = SolverType.Zeidel,
+				Eps = 0.00000001d
 			};
 
-			var method = new EllipticalFiniteDifference(conditions, equation, @params);
+			Array<double> first = Array.Empty<double>();
+			Array<double> errNonLog = Array.Empty<double>();
+			var errors = new double[0, 0];
 
-			var result = method.Solve();
+			for (int i = 4; i <= 80; i += 3)
+			{
+				@params.XStepCount = i;
+				@params.YStepCount = i;
+				var method = new EllipticalFiniteDifference(conditions, equation, @params);
+				var result = method.Solve();
+				errors = method.FindError(result, (x, y) => Math.Exp(y) * Math.Sin(x));
+				var maxError = Math.Log(FindMax(errors));
+				first = first.Append(maxError).ToArray();
+				errNonLog = errNonLog.Append(FindMax(errors)).ToArray();
+			}
 
-			var errors = method.FindError(result, (x, y) => y * Math.Sin(x));
+			errNonLog = errNonLog.T;
+			first = first.T;
 
-			var maxError = FindMax(errors);
-			var median = FindMedian(errors);
-
-			Console.WriteLine($"Max error: {maxError}; median error: {median}");
 			Console.ReadKey();
+
 		}
 	}
 }
